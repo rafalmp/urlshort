f3883742c473aebd68d841a47c76ccf1ac961958
Rafał Peisert <rafalmp@users.noreply.github.com>
2023-04-04 21:12:08 +0200

    Implement MapHandler

---
+++ handler.go
@@ -0,0 +1,23 @@
+package urlshort
+
+import "net/http"
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	//
+	return func(w http.ResponseWriter, r *http.Request) {
+		// if path matches, redirect to it...
+		path := r.URL.Path
+		if destination, found := pathsToUrls[path]; found {
+			http.Redirect(w, r, destination, http.StatusFound)
+			return
+		}
+		// else use fallback
+		fallback.ServeHTTP(w, r)
+	}
+}
--- main/main.go
+++ main/main.go
@@ -3,11 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/rafalmp/urlshort"
 )
 
 func main() {
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+
 	fmt.Println("Starting the server on port 8000")
-	http.ListenAndServe(":8000", defaultMux())
+	http.ListenAndServe(":8000", mapHandler)
 }
 
 func defaultMux() (mux *http.ServeMux) {
